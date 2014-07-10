19c946e3da760461fc52412fcbe7c4af3d484d59
Adrian Gygax <notizklotz@gmx.net>
2014-07-10 22:40:12 +0200

    Use SVG instead of DIVs

--- fiscopoly.js
+++ fiscopoly.js
@@ -3,88 +3,101 @@
 'use strict';
 
 angular.module('fiscopolyApp', [])
-    .controller('BoardCtrl',
-        function($scope) {
-
-    $scope.layout = {
-        width: 65,
-        height: this.width * 1.5,
+    .value('uiConstants', {
+        width: 64,
+        height: 96,
         fieldsPerRow: 9,
-        ribbonHeight: 25,
-        canvasLength: this.fieldsPerRow * this.width + 2 * this.height,
-        backgroundColor: '#CEE7CE',
-        fieldGroupsColors: {
-            brown: '#945531',
-            cyan: '#ADE3FF',
-            magenta: '#DE3894',
-            ochre: '#F79618',
-            red: '#EF1C21',
-            yellow: 'yellow',
-            green: '#18B25A',
-            blue: '#0071BD'
-        }
-    }
+        ribbonHeight: 25
+    })
+    .controller('BoardCtrl',
+        function($scope, uiConstants) {
 
-    function Field(name, number, type, group) {
-        this.name = name;
+    function Field(name1, name2, number, type, group) {
+        this.name1 = name1;
+        this.name2 = name2;
         this.number = number;
         this.type = type;
         this.group = group;
         this.isSystem = function() {
             return this.type === 'system';
         }
+
+        if(number > 30) {
+            this.orientation = 'E';
+            this.x = uiConstants.height + uiConstants.fieldsPerRow * uiConstants.width;
+            this.y = uiConstants.height + (number - 30) * uiConstants.width;
+            this.deg = 270 ;
+        } else if(number > 20) {
+            this.orientation = 'N';
+            this.x = uiConstants.height + (number - 20) * uiConstants.width;
+            this.y = uiConstants.height;
+            this.deg = 180 ;
+        } else if(number > 10) {
+            this.orientation = 'W';
+            this.x = uiConstants.height;
+            this.y = uiConstants.height + (uiConstants.fieldsPerRow - (number - 10)) * uiConstants.width;
+            this.deg = 90;
+        } else {
+            this.orientation = 'S';
+            this.x = uiConstants.height + (uiConstants.fieldsPerRow - number) * uiConstants.width;
+            this.y = uiConstants.height + uiConstants.fieldsPerRow * uiConstants.width;
+            this.deg = 0;
+        }
     }
 
     $scope.fields = [
-        new Field("MONTAG", 0, 'corner'),
-        new Field("SYSTEM", 1, 'system', 'brown'),
-        new Field("KAFFEEPAUSE", 2),
-        new Field("SYSTEM", 3, 'system','brown'),
-        new Field("ZEIT BUCHEN", 4),
-        new Field("SULGENAU", 5, 'station'),
-        new Field("SYSTEM", 6, 'system','cyan'),
-        new Field("NEWSLETTER", 7),
-        new Field("SYSTEM", 8, 'system','cyan'),
-        new Field("SYSTEM", 9, 'system','cyan'),
-        new Field("MEETING", 10, 'corner'),
-        new Field("SYSTEM", 11, 'system','magenta'),
-        new Field("NESPRESSO", 12),
-        new Field("SYSTEM", 13, 'system','magenta'),
-        new Field("SYSTEM", 14, 'system','magenta'),
-        new Field("OBERZOLLIKOFEN", 15, 'station'),
-        new Field("SYSTEM", 16, 'system','ochre'),
-        new Field("KAFFEEPAUSE", 17),
-        new Field("SYSTEM", 18, 'system','ochre'),
-        new Field("SYSTEM", 19, 'system','ochre'),
-        new Field("FEIERABEND", 20, 'corner'),
-        new Field("SYSTEM", 21, 'system','red'),
-        new Field("NEWSLETTER", 22),
-        new Field("SYSTEM", 23, 'system','red'),
-        new Field("SYSTEM", 24, 'system','red'),
-        new Field("WANDER", 25, 'station'),
-        new Field("SYSTEM", 26, 'system','yellow'),
-        new Field("SYSTEM", 27, 'system','yellow'),
-        new Field("JULIO", 28),
-        new Field("SYSTEM", 29, 'system','yellow'),
-        new Field("INS MEETING", 30, 'corner'),
-        new Field("SYSTEM", 31, 'system','green'),
-        new Field("SYSTEM", 32, 'system','green'),
-        new Field("KAFFEEPAUSE", 33),
-        new Field("SYSTEM", 34, 'system','green'),
-        new Field("BERN HB", 35, 'station'),
-        new Field("NEWSLETTER", 36),
-        new Field("STOLIS", 37, 'system','blue'),
-        new Field("ZEIT BUCHEN", 38),
-        new Field("MOLIS", 39, 'system','blue')
+        new Field("MMBU", "", 0, 'corner'),
+        new Field("SYSTEM", "", 1, 'system', 'brown'),
+        new Field("KAFFEE","PAUSE", 2),
+        new Field("SYSTEM", "",3, 'system','brown'),
+        new Field("ZEIT","BUCHEN", 4),
+        new Field("SULGEN-", "AU",5, 'station'),
+        new Field("SYSTEM","", 6, 'system','cyan'),
+        new Field("NEWS-","LETTER", 7),
+        new Field("SYSTEM","", 8, 'system','cyan'),
+        new Field("SYSTEM","", 9, 'system','cyan'),
+        new Field("MEETING","", 10, 'corner'),
+        new Field("SYSTEM","", 11, 'system','magenta'),
+        new Field("NES-","PRESSO", 12),
+        new Field("SYSTEM","", 13, 'system','magenta'),
+        new Field("SYSTEM","", 14, 'system','magenta'),
+        new Field("ZOLLI-","KOFEN", 15, 'station'),
+        new Field("SYSTEM","", 16, 'system','ochre'),
+        new Field("KAFFEE","PAUSE", 17),
+        new Field("SYSTEM","", 18, 'system','ochre'),
+        new Field("SYSTEM","", 19, 'system','ochre'),
+        new Field("FEIERABEND","", 20, 'corner'),
+        new Field("SYSTEM","", 21, 'system','red'),
+        new Field("NEWS-","LETTER", 22),
+        new Field("SYSTEM","", 23, 'system','red'),
+        new Field("SYSTEM","", 24, 'system','red'),
+        new Field("WANDER","", 25, 'station'),
+        new Field("SYSTEM","", 26, 'system','yellow'),
+        new Field("SYSTEM","", 27, 'system','yellow'),
+        new Field("JULIO","", 28),
+        new Field("SYSTEM","", 29, 'system','yellow'),
+        new Field("INS MEETING","", 30, 'corner'),
+        new Field("SYSTEM","", 31, 'system','green'),
+        new Field("SYSTEM","", 32, 'system','green'),
+        new Field("KAFFEE","PAUSE", 33),
+        new Field("SYSTEM","", 34, 'system','green'),
+        new Field("BERN HB","", 35, 'station'),
+        new Field("NEWS-","LETTER", 36),
+        new Field("STOLIS","", 37, 'system','blue'),
+        new Field("ZEIT","BUCHEN", 38),
+        new Field("MOLIS","", 39, 'system','blue')
     ]})
-    .directive('field', function() {
+    .directive('field', function(uiConstants, $log) {
         return {
             restrict: 'E',
             replace: true,
             scope: {
                 field: '='
             },
-            templateUrl: 'field.html'
+            templateUrl: 'field.html',
+            link: function (scope, element) {
+                scope.showRibbon = scope.field.isSystem();
+            }
         };
     })
     ;
